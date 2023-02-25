99d874d2f1157956a827372f829ce7e7bad66d10
slowisfast <slowisfast2030@163.com>
2023-02-25 23:12:57 +0800

    all is well

---
+++ asyc/youtube.js
@@ -0,0 +1,21 @@
+// promise可以保证命令执行的order
+
+function makeRequest(location) {
+    return new Promise((resolve, reject) => {
+        console.log(`Making Request to ${location}`)
+        if (location === 'Google'){
+            resolve('Google say hi')
+        } else {
+            reject('We can only talk to Google')
+        }
+    })
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing responce')
+        resolve(`Extra Information + ${response}`)
+    })
+}
+
+makeRequest('Google')

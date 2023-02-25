09484cbb85df1935281fdfdd3ff629d7b6cdc0c8
slowisfast <slowisfast2030@163.com>
2023-02-26 00:21:22 +0800

    all is well

--- asyc/fetch.js
+++ asyc/fetch.js
@@ -9,3 +9,16 @@ fetch(url)
     .catch(err => {
         console.error(err)
     })
+
+// promise的then语句相比于回调函数的使用已经简洁了许多
+// 但是还可以进一步简化，通过使用async和await
+
+const loadData = async () => {
+    try{
+        
+
+    }catch(err) {
+
+    }
+
+}
